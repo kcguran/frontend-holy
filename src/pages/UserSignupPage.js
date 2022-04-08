26a6db9e684be543d27c91e1ab06c13ed1775696
Kerem Can <keremcangurann@hotmail.com>
2022-04-09 02:37:05 +0300

    Validation Errors

--- src/pages/UserSignupPage.js
+++ src/pages/UserSignupPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { signUp } from '../api/apiCalls'
+import Input from '../components/input';
 
 class UserSignupPage extends React.Component {
 
@@ -8,14 +9,18 @@ class UserSignupPage extends React.Component {
         displayName: null,
         password: null,
         passwordRepeat: null,
-        pendingApiCall: false
+        pendingApiCall: false,
+        errors: {}
     }
 
     onChange = event => {
 
         const { name, value } = event.target;
+        const errors = { ...this.state.errors }
+        errors[name] = undefined
         this.setState({
-            [name]: value
+            [name]: value,
+            errors
         })
     }
 
@@ -32,25 +37,24 @@ class UserSignupPage extends React.Component {
         try {
             const response = await signUp(body);
         } catch (error) {
+            if (error.response.data.validationErrors) {
+                this.setState({ errors: error.response.data.validationErrors })
+            }
         }
-        this.setState({ pendingApiCall: false})
+        this.setState({ pendingApiCall: false })
     };
 
 
     render() {
-        const { pendingApiCall } = this.state;
+        const { pendingApiCall, errors } = this.state;
+        const { username, displayName, password } = errors;
         return (
             <div className="container">
                 <form>
                     <h1 className="text-center">Sign Up</h1>
-                    <div className="form-group">
-                        <label>Username</label>
-                        <input className="form-control" name="username" onChange={this.onChange} />
-                    </div>
-                    <div className="form-group">
-                        <label>Display Name</label>
-                        <input className="form-control" name="displayName" onChange={this.onChange} />
-                    </div>
+                    <Input name="username" label="Username" error={username} onChange={this.onChange} />
+                    <Input name="displayName" label="Display Name" error={displayName} onChange={this.onChange} />
+                    <Input name="password" label="Password" error={password} onChange={this.onChange} type="password" />
                     <div className="form-group">
                         <label>Password</label>
                         <input className="form-control" name="password" onChange={this.onChange} type="password" />

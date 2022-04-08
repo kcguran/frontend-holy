682bc01d8c904985bae11b5d5d0ac8c2068a7428
Kerem Can <keremcangurann@hotmail.com>
2022-04-08 03:54:02 +0300

    Added bootstrap and conditional rendering

--- src/UserSignupPage.js
+++ src/UserSignupPage.js
@@ -1,45 +1,73 @@
 import React from "react";
+import axios from "axios";
 
-class UserSignupPage extends React.Component{
+class UserSignupPage extends React.Component {
 
     state = {
         username: null,
         displayName: null,
         password: null,
-        passwordRepeat: null
+        passwordRepeat: null,
+        pendingApiCall: false
     }
 
     onChange = event => {
-        const value = event.target.value;
-        const field = event.target.name;
+
+        const { name, value } = event.target;
         this.setState({
-            [field]: value
+            [name]: value
         })
     }
 
+    onClickSignup = event => {
+        event.preventDefault();
+        const { username, displayName, password } = this.state;
+        const body = {
+            username,
+            displayName,
+            password
+        };
+        this.setState({ pendingApiCall: true });
+        axios.post('/api/1.0/users', body)
+            .then(response => {
+                this.setState({ pendingApiCall: false });
+            }).catch(error => {
+                this.setState({ pendingApiCall: false })
+            });
+    };
+
 
     render() {
-        return(
-            <form>
-                <h1>Sign Up</h1>
-                <div>
-                <label>Username</label>
-                <input name="username" onChange={this.onChange} />
-                </div>
-                <div>
-                <label>Display Name</label>
-                <input name="displayName" onChange={this.onChange}/>
-                </div>
-                <div>
-                <label>Password</label>
-                <input name="password" onChange={this.onChange} type="password"/>
-                </div>
-                <div>
-                <label>Password Repeat</label>
-                <input name="passwordRepeat" onChange={this.onChange} type="password"/>
-                </div>
-                <button>Sign Up</button>
-            </form>
+        return (
+            <div className="container">
+                <form>
+                    <h1 className="text-center">Sign Up</h1>
+                    <div className="form-group">
+                        <label>Username</label>
+                        <input className="form-control" name="username" onChange={this.onChange} />
+                    </div>
+                    <div className="form-group">
+                        <label>Display Name</label>
+                        <input className="form-control" name="displayName" onChange={this.onChange} />
+                    </div>
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input className="form-control" name="password" onChange={this.onChange} type="password" />
+                    </div>
+                    <div className="form-group">
+                        <label>Password Repeat</label>
+                        <input className="form-control" name="passwordRepeat" onChange={this.onChange} type="password" />
+                    </div>
+                    <div className="text-center">
+                        <button
+                            className="btn btn-primary mt-2"
+                            onClick={this.onClickSignup}
+                            disabled={this.state.pendingApiCall}>
+                            {this.state.pendingApiCall && <span className="spinner-border spinner-border-sm"></span>} Sign Up
+                        </button>
+                    </div>
+                </form>
+            </div>
         )
     };
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import './bootstrap-override.scss'
 import UserSignupPage from './UserSignupPage';
 import reportWebVitals from './reportWebVitals';
 

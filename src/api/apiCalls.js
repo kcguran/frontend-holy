b56c79362fc35d455dc516e0b02b347c873178e3
Kerem Can <keremcangurann@hotmail.com>
2022-04-09 05:27:10 +0300

    Basic Auth

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -1,9 +1,14 @@
 import axios from "axios"
 
 export const signUp = (body) => {
-    return axios.post('/api/1.0/users', body)
+    return axios.post('/api/1.0/users', body);
 };
 
-export const changeLanguage = language =>{
+export const login = creds => {
+    return axios.post('/api/1.0/auth', {}, { auth: creds });
+}
+
+
+export const changeLanguage = language => {
     axios.defaults.headers['accept-language'] = language;
 }

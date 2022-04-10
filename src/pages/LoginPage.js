2e17a4377f164be1158fc861edc479d1768440bc
Kerem Can <keremcangurann@hotmail.com>
2022-04-11 02:21:10 +0300

    Exception Messages

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -7,34 +7,53 @@ class LoginPage extends Component {
     state = {
         username: null,
         password: null,
+        error: null
     }
 
     onChange = event => {
         const { name, value } = event.target;
         this.setState({
-            [name]: value
+            [name]: value,
+            error: null
         })
     }
 
-    onClickLogin = event => {
+    onClickLogin = async event => {
         event.preventDefault();
         const { username, password } = this.state;
         const creds = {
             username,
             password
+        };
+        this.setState({
+            error: null
+        })
+        try {
+            await login(creds);
+        } catch (apiError) {
+            this.setState({
+                error: apiError.response.data.message
+            })
         }
-        login(creds);
     }
 
     render() {
+        const { username, password, error } = this.state;
+        const buttonEnabled = username && password;
         return (
             <div className='container'>
                 <h1 className='text-center'>Login</h1>
                 <form>
                     <Input label="Username" name="username" onChange={this.onChange} />
                     <Input label="password" name="password" type="password" onChange={this.onChange} />
+                    {this.state.error && <div className='alert alert-danger'>{error}</div>}
                     <div className='text-center mt-2'>
-                        <button className='btn btn-primary' onClick={this.onClickLogin}>Login</button>
+                        <button className='btn btn-primary'
+                            onClick={this.onClickLogin}
+                            disabled={!buttonEnabled}
+                        >
+                            Login
+                        </button>
                     </div>
                 </form>
             </div>
